88a393a0c11d8284ebb0d5416df4b999d0a62aba
Yahya <yahyaabbakar@gmail.com>
2023-01-18 17:41:03 +0200

    added local storage

--- script.js
+++ script.js
@@ -45,12 +45,24 @@ function lightMode() {
 function switchTheme(e) {
 	if (e.target.checked) {
 		document.documentElement.setAttribute('data-theme', 'dark');
+		localStorage.setItem('theme', 'dark');
 		darkMode();
 	} else {
 		document.documentElement.setAttribute('data-theme', 'light');
+		localStorage.setItem('theme', 'light');
 		lightMode();
 	}
 }
 
 // Event Listener
 toggleSwitch.addEventListener('change', switchTheme);
+
+// Check Local Storage for Theme
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+	document.documentElement.setAttribute('data-theme', currentTheme);
+	if (currentTheme === 'dark') {
+		toggleSwitch.checked = true;
+		darkMode();
+	}
+}
